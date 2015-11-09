39a764fa3d892d2a7afcccbbd5fe44c174310b7c
Lukas Nico Sahin Zamora Jabre <lzamora@ip136.9.priv.inf.utfsm.cl>
2015-11-09 18:53:29 -0300

    Actualizacion main

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include "funciones.c"
 #include "nodes.h"
+#define MAX_BUFFER 1000
 
 int main(){
+	int n_comandos;
+	printf("");
+	scanf("%d",&n_comandos);
+	char string[MAX_BUFFER];
+	char *ptrstring;
+	ptrstring=string;
+	for(int i=0;i<n_comandos;i++){
+		printf(">> ");
+		scanf("%s", ptrstring);
+		getchar();
+		puts(ptrstring);
+	}
 
-
-  return 0;
+	return 0;
 }
