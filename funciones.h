64a63841f327d2e2b5ada63fdeaef50c86dc581d
diego andres wilhelm villavicencio <dwilhelm@tongoy.labs.inf.utfsm.cl>
2015-11-09 18:04:11 -0300

    Actualizados main, funciones

--- funciones.h
+++ funciones.h
@@ -1,7 +1,16 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char **split(char *phrase, const size_t length, const char delimiter, size_t *n_tokens);
+
 char *linea_archivo(FILE *archivo);
 
 FILE *leer_archivo(char *string);
 
 int string_to_int(char *string);
-//chupala123...o
-//diego ql 
+
+#endif
